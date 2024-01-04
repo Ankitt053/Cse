b64c9dd52351eca72623b0d40ecfdd00706c6197
Ankit <146940547+Ankitt053@users.noreply.github.com>
2024-01-04 20:19:12 +0530

    Arithmetic Operations.c

---
+++ Arithmetic Operations.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum=%d\n",c);
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("Difference=%d\n",c);
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    printf("Product=%d\n",c);
+    scanf("%d%d",&a,&b);
+    c=a/b;
+    printf("Quoitent=%d\n",c);
+    scanf("%d%d",&a,&b);
+    return 0;
+} 
+
+
+
